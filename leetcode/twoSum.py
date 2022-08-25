4d17289638be0ee52189a649788a20661be35d98
tcellcode <99007786+tcellcode@users.noreply.github.com>
2022-08-25 07:47:05 +0700

    Create twoSum.py

---
+++ leetcode/twoSum.py
@@ -0,0 +1,8 @@
+def twoSum(nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+              
