97e67620603432460d585948a3aac1ccebc5f698
tcellcode <99007786+tcellcode@users.noreply.github.com>
2022-08-28 22:08:46 +0700

    Update hash_table.py
    add custom exception

--- data structure/hash_table.py
+++ data structure/hash_table.py
@@ -1,15 +1,20 @@
+class HashTableError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
 class HashTable:
-    def __init__(self, size=10):
+    def __init__(self, size=10) -> None:
         self.size = size
         self.table = [[] for _ in range(self.size)]
         
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.table)
         
     def hash_func(self, key):
-        return hash(key) % self.size # insert custom hash method here 
-
-            
+        return hash(key) % self.size
+      
     def insert(self, key, value):
         hash_idx = self.hash_func(key)
         bucket = self.table[hash_idx]
@@ -18,25 +23,30 @@ class HashTable:
             if item[0] == key:
                 item[1] = value
                 return
+            
         bucket.append([key, value])
             
         
-    def find(self, key):
+    def search(self, key):
         hash_idx = self.hash_func(key)
         bucket = self.table[hash_idx]
         
         for item in bucket:
             if item[0] == key:
                 return item[1]
-        
-def main():
-    square = HashTable(size=2)
-    square.insert("color", "blue")
-    square.insert("side", "4")
-    square.insert("name", "Timmy")
-    print(square.find("name"))
     
-if __name__ == "__main__":
-    main()
-
+    def delete(self, key):
+        hash_idx = self.hash_func(key)
+        bucket = self.table[hash_idx]
+        key_exist = False
+        
+        for idx, item in enumerate(bucket):
+            if item[0] == key:
+                key_exist = True
+        
+        if key_exist:
+            del bucket[idx]
+        else:
+            raise HashTableError(f"Key {key} not existed!")
+    
 
