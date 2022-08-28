7df4fd5daf701bebe5958ca183cdbdd220dff555
tcellcode <99007786+tcellcode@users.noreply.github.com>
2022-08-28 12:39:38 +0700

    Update flatten_list.py

--- algorithm/flatten_list.py
+++ algorithm/flatten_list.py
@@ -1,7 +1,7 @@
 # can flatten irregular list
-def flatten(l):
-    if len(l) == 0:
-        return l
-    if type(l[0]) in (list, tuple, set):
-        return flatten(l[0]) + flatten(l[1:])
-    return l[:1] + flatten(l[1:])
+def flatten(L):
+    for item in L:
+        try:
+            yield from flatten(item)
+        except TypeError:
+            yield item
