3063ed967325aa499d8e19743d85cd7df28c933a
tcellcode <99007786+tcellcode@users.noreply.github.com>
2022-08-29 22:41:36 +0700

    Update dot.py

--- algorithm/sort/insertion sort/dot.py
+++ algorithm/sort/insertion sort/dot.py
@@ -2,8 +2,8 @@
 def insertion_sort(arr, reverse=False):
     for i in range(1, len(arr)):
         for j in range(i, 0, -1):
-            state = arr[j] > arr[j-1] if reverse else arr[j] < arr[j-1]
-            if state:
+            sort_order = arr[j] > arr[j-1] if reverse else arr[j] < arr[j-1]
+            if sort_order:
                 arr[j], arr[j-1] = arr[j-1], arr[j]
             else: break
                 
