40b622537945dca9ae32034cc4cacfe1cebf6864
tcellcode <99007786+tcellcode@users.noreply.github.com>
2022-08-29 22:40:35 +0700

    Update dot.cpp
    rename state to sort_order

--- algorithm/sort/insertion sort/dot.cpp
+++ algorithm/sort/insertion sort/dot.cpp
@@ -1,12 +1,12 @@
 // return a pointer to arr
 int* insertion_sort(int arr[], int arr_len, bool reverse = false){
     int temp;
-    bool state;
+    bool sort_order;
     for (int i = 1; i < arr_len; i++){
         for (int j = i; j > 0; j--){
-            state = (reverse == true)? arr[j] > arr[j-1] : arr[j] < arr[j-1];
+            sort_order = (reverse == true)? arr[j] > arr[j-1] : arr[j] < arr[j-1];
 
-            if (state){
+            if (sort_order){
                 temp = arr[j-1];
                 arr[j-1] = arr[j];
                 arr[j] = temp;
